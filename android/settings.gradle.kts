ac0d90b67284db9390aa21406ea0928349c9abc6
khosoof <ahmad3ta1@yahoo.com>
2025-09-01 02:40:57 +0330

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,20 +1,11 @@
+// android/settings.gradle.kts
 pluginManagement {
-    // این خط، بیلد سیستم فلاتر را به پروژه اندروید وصل می‌کند
-    includeBuild("../..")
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
-
-plugins {
-    // لودر پلاگین فلاتر (لازم)
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -22,6 +13,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 rootProject.name = "salon_manager"
 include(":app")
