579c99be8ca26562b3fd20b7367652da0df0078b
OTOX <ahmad3ta1@yahoo.com>
2025-09-01 23:27:06 +0330

    test: global config for EasyLocalization/intl and golden fonts

---
+++ test/flutter_test_config.dart
@@ -0,0 +1,22 @@
+﻿import 'dart:async';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:intl/intl.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  // لازم برای هر تست ویجت
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  // زبان پیش‌فرض برای intl (فرمت پول و …)
+  Intl.defaultLocale = 'fa';
+
+  // آماده‌سازی easy_localization (برای tr())
+  await EasyLocalization.ensureInitialized();
+
+  // فونت‌ها برای تست‌های گلدن
+  await loadAppFonts();
+
+  // حالا تست‌ها را اجرا کن
+  await testMain();
+}
