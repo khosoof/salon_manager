b539881ff1493cec37e9bde5c3fc51a7190fc2f2
OTOX <ahmad3ta1@yahoo.com>
2025-09-02 00:38:19 +0330

    test: fix flutter_test_config and add golden baselines

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -2,10 +2,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/intl.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  // Locale پیش‌فرض برای عدد/تاریخ
   Intl.defaultLocale = 'fa';
+
+  // Mock in-memory برای shared_preferences (بدون علامت ?)
+  SharedPreferences.setMockInitialValues(<String, Object>{});
+
+  // init برای EasyLocalization
   await EasyLocalization.ensureInitialized();
+
   await testMain();
 }
